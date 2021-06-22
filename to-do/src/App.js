21474832cb6771075d68c2f8c0623881de88a236
percius47 <33738242+percius47@users.noreply.github.com>
2021-06-22 19:15:37 +0530

    pck update

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import TextField from '@material-ui/core/TextField';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Prashant's to-do App</h1>
+      <h2>hello</h2>
+      <TextField id="standard-basic" className="textField" label="To-Do" />
     </div>
   );
 }
